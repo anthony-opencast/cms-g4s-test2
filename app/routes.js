68ee4f19a34a002abe0cc5f2c7be6fac21b3a28d
Anthony E <anthony.elstob@opencastsoftware.com>
2021-08-25 19:33:49 +0100

    Import of G4S Test 2 from main prototype

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+const g4s2 = require('./routes/g4s2');;
+
+router.use(g4s2);
 
 module.exports = router
